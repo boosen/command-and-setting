7f52121a2f0bdda7f2aeaf1ac28179fa627677e9
Bushen Shi <bushen.shi@hotmail.com>
2016-03-08 09:39:07 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+echo -e '/var/lib/docker/aufs/mnt/49eb700db6e29b3916bdc076773521c784921e8996f22353ab7e29cbe3ccdbbc/home/boosen'
+
 docker run -ti -d --name web -v /usr/local/container/data:/usr/local/mysql/data -v /var/www:/var/www -v /usr/local/container/nginx_conf:/usr/local/nginx/conf -p 80:80 -p 3306:3306 boosen/lnmp /bin/bash
